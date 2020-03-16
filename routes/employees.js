82bc5161475a53c5ecec754ed0bb85927e7a5d95
graymatter623 <scarpenter@bestpeers.com>
2020-03-16 18:49:08 +0530

    Employee fetch api updated

--- routes/employees.js
+++ routes/employees.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const Employee = require('../models/employee');
 const router = express.Router();
-//const checkToken = require('../token-authorization/checkToken');
-router.get('/',(req,res)=>{
-    res.json({employee : {
-        name : "NAME",
-        username : "UNNAME"
-    }});
-    // Employee.find().then((employee)=>{
-    //     console.log('Employees Fetched');
-    //     if(employee) res.status(200).json({success : true , employee : employee});
-    // }).catch(error=>{
-    //     if(error) res.status(404).json({error, success : false});
-    // });
+const checkToken = require('../token-authorization/checkToken');
+router.get('/',checkToken.checkToken,(req,res)=>{
+    Employee.find().then((employee)=>{
+        console.log('Employees Fetched');
+        if(employee) res.status(200).json({success : true , employee : employee});
+    }).catch(error=>{
+        if(error) res.status(404).json({error, success : false});
+    });
 });
 module.exports = router;
