2b51a3fc220681dd545599c4dfde2b7d6b58882c
graymatter623 <scarpenter@bestpeers.com>
2020-03-16 18:26:50 +0530

    removed check token from eployees

--- routes/employees.js
+++ routes/employees.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const Employee = require('../models/employee');
 const router = express.Router();
-const checkToken = require('./token-authorization/checkToken');
-router.get('/',checkToken.checkToken,(req,res)=>{
-    Employee.find((error,employee)=>{
+const checkToken = require('../token-authorization/checkToken');
+router.get('/',(req,res)=>{
+    Employee.find().then((employee)=>{
         console.log('Employees Fetched');
-        if(error){
-            return res.status(500).json({error, success : false});
-        }
-        res.status(200).json({success : true , employee : employee});
+        if(employee) res.status(200).json({success : true , employee : employee});
+    }).catch(error=>{
+        if(error) res.status(404).json({error, success : false});
     });
 });
 module.exports = router;
